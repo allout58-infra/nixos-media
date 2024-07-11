4c91b5277ddb1e68e0731eb941441cab24179687
James Hollowell <jamesthollowell@gmail.com>
2024-07-10 21:10:43 -0400

    fix: add dns resolver so lego finds the zone correctly

--- reverse-proxy.nix
+++ reverse-proxy.nix
@@ -5,13 +5,14 @@
       defaults.email = "jamesthollowell@gmail.com";
       certs = let cloudflare_dns_chalenge = {
         dnsProvider = "cloudflare";
+        # Have to specify an external resolver, otherwise LEGO (the ACME client) thinks `home.jameshollowell.com` is a valid zone.
+        # See https://github.com/traefik/traefik/issues/3585 and https://github.com/go-acme/lego/issues/570
+        dnsResolver = "1.1.1.1:53";
         environmentFile = config.age.secrets."cloudflare-dns-challenge".path;
       }; in {
         "jellyfin.home.jameshollowell.com" = cloudflare_dns_chalenge;
         "jellyseerr.home.jameshollowell.com" = cloudflare_dns_chalenge;
       };
-      # Temporarily use staging server for testing.
-      defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     };
 
     services.nginx = {
