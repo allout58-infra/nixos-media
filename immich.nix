140e0f5a06c6e7aa2e3e106317ffb203510b3d38
James Hollowell <jamesthollowell@gmail.com>
2024-10-14 22:28:42 -0400

    fix: move immich media location

--- immich.nix
+++ immich.nix
@@ -25,6 +25,7 @@
     environment.IMMICH_MACHINE_LEARNING_URL = "http://localhost:3003";
     openFirewall = true;
     host = "0.0.0.0";
+    mediaLocation = "/mnt/data/pictures";
   };
 
   users.users.immich.extraGroups = ["video" "render"];
--- media-mnt.nix
+++ media-mnt.nix
@@ -4,4 +4,9 @@
     fsType = "nfs";
     options = ["auto" "nofail" "noatime" "nolock" "intr" "tcp" "actimeo=1800"];
   };
+  fileSystems."/mnt/data" = {
+    device = "192.168.2.20:/mnt/tank/data";
+    fsType = "nfs";
+    options = ["auto" "nofail" "noatime" "nolock" "intr" "tcp" "actimeo=1800"];
+  };
 }
