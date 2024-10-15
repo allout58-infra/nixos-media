e540076a138adefccbd9b385c3619c098821a884
James Hollowell <jamesthollowell@gmail.com>
2024-10-14 22:56:51 -0400

    Revert "feat: try to change up pictures mount point"
    This reverts commit 5855ff078c347ec8a7904ab21ac96b4b67c744aa.

--- media-mnt.nix
+++ media-mnt.nix
@@ -4,8 +4,8 @@
     fsType = "nfs";
     options = ["auto" "nofail" "noatime" "nolock" "intr" "tcp" "actimeo=1800"];
   };
-  fileSystems."/mnt/pictures" = {
-    device = "192.168.2.20:/mnt/tank/data/pictures";
+  fileSystems."/mnt/data" = {
+    device = "192.168.2.20:/mnt/tank/data";
     fsType = "nfs";
     options = ["auto" "nofail" "noatime" "nolock" "intr" "tcp" "actimeo=1800"];
   };
