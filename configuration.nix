55568f49c23c1a976c17bcd28341836da5576583
James Hollowell <jamesthollowell@gmail.com>
2025-01-29 11:29:58 -0500

    feat: set nh default flake

--- configuration.nix
+++ configuration.nix
@@ -21,4 +21,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  programs.nh.flake = "/home/jhollowell/nix-media";
 }
