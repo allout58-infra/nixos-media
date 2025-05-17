f2d48f2fe0f8442279f3d3f737aa3745e96e879b
James Hollowell <jamesthollowell@gmail.com>
2025-05-16 23:07:54 -0400

    feat: add libation

--- audiobookshelf.nix
+++ audiobookshelf.nix
@@ -1,6 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   services.audiobookshelf = {
     enable = true;
     openFirewall = true;
+    host = "0.0.0.0";
   };
+  environment.systemPackages = [pkgs.libation];
 }
