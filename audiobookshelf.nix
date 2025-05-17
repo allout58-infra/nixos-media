b3082965e6299d660467b64aca903448712ea5c1
James Hollowell <jamesthollowell@gmail.com>
2025-05-16 23:07:54 -0400

    feat: attempt to add service to run libation every hour

--- audiobookshelf.nix
+++ audiobookshelf.nix
@@ -1,6 +1,41 @@
-{...}: {
+{pkgs, ...}: {
   services.audiobookshelf = {
     enable = true;
     openFirewall = true;
+    host = "0.0.0.0";
+  };
+  environment.systemPackages = [pkgs.libation];
+
+  systemd.services.libation = {
+    description = "Libation";
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "libation";
+      Group = "libation";
+    };
+    script = ''
+      #!/usr/bin/env bash
+      set -euo pipefail
+      exec ${pkgs.libation}/bin/libationcli scan
+      exec ${pkgs.libation}/bin/libationcli liberate
+    '';
+  };
+  systemd.timers.libation = {
+    description = "Libation Timer";
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "*-*-* *:00:00";
+      Persistent = true;
+      Unit = "libation.service";
+    };
+  };
+  users.users.libation = {
+    isSystemUser = true;
+    home = "/var/lib/libation";
+    createHome = true;
+    group = "libation";
+  };
+  users.groups.libation = {
   };
 }
