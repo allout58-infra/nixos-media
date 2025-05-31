7a27666dab3f46570b793ca46a7083e37c4af1b7
James Hollowell <jamesthollowell@gmail.com>
2025-05-31 12:14:52 -0400

    feat: add timer to move libation output to NFS

--- audiobookshelf.nix
+++ audiobookshelf.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.audiobookshelf = {
     enable = true;
     openFirewall = true;
@@ -16,6 +16,30 @@
         "/var/lib/libation:/config"
         "/tmp/audiobooks:/data"
       ];
+    };
+  };
+
+  # Libation had issues pulling directly to the mount point, so we pull to a temporary directory and then move it later
+  systemd.services.libation-pull = {
+    description = "Pull audiobooks from Libation";
+    wants = [ "podman-libation.service" ];
+    after = [ "network.target" "podman-libation.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "root";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'mv /tmp/audiobooks/* /mnt/media/media/Audio\ Books/ && rm -rf /tmp/audiobooks/*'";
+    };
+  };
+  systemd.timers.libation-pull = {
+    description = "Timer to pull audiobooks from Libation";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5min";
+      OnUnitActiveSec = "1h";
+    };
+    unitConfig = {
+      PartOf = "libation-pull.service";
     };
   };
 }
