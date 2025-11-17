d588cde910255cb64929aab0725acbea98a42b0a
James Hollowell <jamesthollowell@gmail.com>
2025-11-16 22:14:43 -0500

    feat: flake update and copy changes from PR for ErsatzTV

--- temp-pkgs/ersatztv.nix
+++ temp-pkgs/ersatztv.nix
@@ -3,9 +3,10 @@
   pkgs,
   lib,
   ...
-}: let
-  inherit
-    (lib)
+}:
+
+let
+  inherit (lib)
     mkIf
     getExe
     maintainers
@@ -15,12 +16,13 @@
     ;
   inherit (lib.types) str path bool;
   cfg = config.services.ersatztv;
-in {
+in
+{
   options = {
     services.ersatztv = {
       enable = mkEnableOption "ErsatzTV";
 
-      package = mkPackageOption pkgs "ersatztv" {};
+      package = mkPackageOption pkgs "ersatztv" { };
 
       user = mkOption {
         type = str;
@@ -34,19 +36,11 @@ in {
         description = "Group under which ErsatzTV runs.";
       };
 
-      transcodeDir = mkOption {
-        type = path;
-        default = "/var/lib/etv-transcode";
-        description = ''
-          Temporary transcode directory
-        '';
-      };
-
-      configDir = mkOption {
+      stateDir = mkOption {
         type = path;
         default = "/var/lib/ersatztv";
         description = ''
-          Directory containing the server configuration files and SQLite Database file
+          State directory for ErsatzTV
         '';
       };
 
@@ -71,34 +65,30 @@ in {
   config = mkIf cfg.enable {
     systemd = {
       tmpfiles.settings.ersatztvDirs = {
-        "${cfg.transcodeDir}"."d" = {
-          mode = "700";
-          inherit (cfg) user group;
-        };
-        "${cfg.configDir}"."d" = {
+        "${cfg.stateDir}"."d" = {
           mode = "700";
           inherit (cfg) user group;
         };
       };
       services.ersatztv = {
         description = "ErsatzTV";
-        after = ["network-online.target"];
-        wants = ["network-online.target"];
-        wantedBy = ["multi-user.target"];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        wantedBy = [ "multi-user.target" ];
 
         serviceConfig = {
           Type = "simple";
           User = cfg.user;
           Group = cfg.group;
           UMask = "0077";
-          WorkingDirectory = cfg.configDir;
+          WorkingDirectory = cfg.stateDir;
           ExecStart = getExe cfg.package;
           Restart = "on-failure";
         };
 
         environment = {
-          ETV_CONFIG_FOLDER = cfg.configDir;
-          ETV_TRANSCODE_FOLDER = cfg.transcodeDir;
+          ETV_CONFIG_FOLDER = "${cfg.stateDir}/config";
+          ETV_TRANSCODE_FOLDER = "${cfg.stateDir}/transcode";
           ETV_BASE_URL = cfg.baseUrl;
         };
       };
@@ -111,13 +101,14 @@ in {
       };
     };
 
-    users.groups = mkIf (cfg.group == "ersatztv") {ersatztv = {};};
+    users.groups = mkIf (cfg.group == "ersatztv") { ersatztv = { }; };
 
     networking.firewall = mkIf cfg.openFirewall {
       # from https://ersatztv.org/docs/user-guide/install#manual-installation-2
-      allowedTCPPorts = [8409];
+      allowedTCPPorts = [ 8409 ];
     };
+
   };
 
-  meta.maintainers = with maintainers; [allout58];
+  meta.maintainers = with maintainers; [ allout58 ];
 }
