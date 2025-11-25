52c8d4af13b8d56ef103888497a5b6d4a708e8e5
James Hollowell <jamesthollowell@gmail.com>
2025-11-24 21:21:58 -0500

    feat: try patch for service

--- ersatztv.nix
+++ ersatztv.nix
@@ -1,10 +1,9 @@
 {...}: {
+  imports = [
+    ./temp-pkgs/ersatztv.nix
+  ];
   services.ersatztv = {
     enable = true;
     openFirewall = true;
-    environment = {
-      ETV_BASE_URL = "/";
-      ETV_UI_PORT = "8409";
-    };
   };
 }
